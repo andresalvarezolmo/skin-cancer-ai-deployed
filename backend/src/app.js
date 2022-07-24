9af9d4fa24176d4311ca5ef580048345183e5828
Andres <andresalo2011@gmail.com>
2022-07-24 14:38:54 +0000

    model is loaded in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const tf = require('@tensorflow/tfjs');
+require('@tensorflow/tfjs-node');
 
 const app = express();
-const httpServer = createServer(app);
-const io = new Server(httpServer, 
-  { cors:{
-    origin: "*"
-    } 
-});
+const server = createServer(app);
+const io = new Server(server,
+  {
+    cors: {
+      origin: "*"
+    }
+  });
+
 const port = process.env.PORT || 5000;
 
 io.on("connection", (socket) => {
@@ -18,12 +22,22 @@ io.on("connection", (socket) => {
     socket.emit("pong");
   })
 
-  socket.on("disconnect", function (){
+  socket.on("disconnect", function () {
     console.log("terminate socket")
     socket.disconnect(0);
+  });
 });
+
+
+app.get('/', (req, res) => {
+  res.send("Hello world")
+})
+app.get('/predict', async (req, res) => {
+  console.log("Cargando modelo...");
+  const model = await tf.loadGraphModel("file://C:/Users/andres/Documents/code/skin-cancer-ai-deployed/backend/src/ml/model.json");
+  console.log("Modelo cargado...");
+})
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
 });
-
-
-
-io.listen(port, () => `Sitting at localhost:${port}`);
