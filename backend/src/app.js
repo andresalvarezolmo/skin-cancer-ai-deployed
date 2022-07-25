fad57e2c82b60302f4c6a51f79ada549d572ffe1
Andres <andresalo2011@gmail.com>
2022-07-25 15:14:40 +0000

    separate routes and first failed test, wrong input shape

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
-const tf = require('@tensorflow/tfjs');
-require('@tensorflow/tfjs-node');
+
+const diagnoseRouter = require('./routes/diagnose');
 
 const app = express();
 const server = createServer(app);
@@ -15,27 +15,20 @@ const io = new Server(server,
 
 const port = process.env.PORT || 5000;
 
-io.on("connection", (socket) => {
-  console.log(`Connected with session ${socket.id}`);
+app.use('/diagnose', diagnoseRouter);
 
+io.on("connection", (socket) => {
   socket.on("ping", () => {
     socket.emit("pong");
   })
 
   socket.on("disconnect", function () {
-    console.log("terminate socket")
     socket.disconnect(0);
   });
 });
 
-
 app.get('/', (req, res) => {
   res.send("Hello world")
-})
-app.get('/predict', async (req, res) => {
-  console.log("Cargando modelo...");
-  const model = await tf.loadGraphModel("file://C:/Users/andres/Documents/code/skin-cancer-ai-deployed/backend/src/ml/model.json");
-  console.log("Modelo cargado...");
 })
 
 server.listen(3000, () => {
--- backend/src/routes/diagnose.js
+++ backend/src/routes/diagnose.js
@@ -1,6 +1,35 @@
 const router = require('express').Router();
+const tf = require('@tensorflow/tfjs');
+const tfnode = require('@tensorflow/tfjs-node');
+const fs = require('fs');
 
-router.get('/', async(req, res) =>{
+// const example1 = require("/Users/andres/Documents/code/skin-cancer-ai-deployed/backend/src/ml/examples/example1.jpg")
+
+
+
+
+router.get('/', async (req, res) => {
+    console.log("Cargando modelo...");
+    const model = await tf.loadGraphModel("file://C:/Users/andres/Documents/code/skin-cancer-ai-deployed/backend/src/ml/model.json");
+    console.log("Modelo cargado...");
     res.send('Diagnose section!')
+
+
+    fs.readFile('src/ml/examples/example1.jpg', (err, buffer) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        // console.log(data);s
+        const image = Buffer.from(buffer, 'base64')
+        // get the tensor
+        const tensor = tfnode.node.decodeImage(image)
+        reshapedTensor = tensor.reshape([1, 1024, 1024, 3]);
+        console.log(reshapedTensor)
+        let prediction = model.predict(reshapedTensor).dataSync();
+        // console.log(prediction)
+    });
+
 })
+
 module.exports = router;
